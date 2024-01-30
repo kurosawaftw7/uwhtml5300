088c4c2b1009e04443abd8d6508d40bdb716a543
kurosawaftw7 <jdharman@gmail.com>
2024-01-29 20:53:34 -0800

    accordion, v-for loop, array added

--- vue3assign7/hello-world/src/App.vue
+++ vue3assign7/hello-world/src/App.vue
@@ -1,10 +1,29 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> 
-    <router-link to="/info">Info</router-link>
+  
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <router-link to="/" class="nav-link">Jeff</router-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/about" class="nav-link">About</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/info" class="nav-link">Info</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
   </nav>
-  <router-view/>
+  <div class="container">
+    <router-view/>
+  </div>
 </template>
 
 <style>
