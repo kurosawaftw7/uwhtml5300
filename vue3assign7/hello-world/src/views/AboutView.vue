7094040a760d927581c149b622a784abb576b7c5
kurosawaftw7 <jdharman@gmail.com>
2024-01-30 18:34:47 -0800

    updated v-for and array, needs eval

--- vue3assign7/hello-world/src/views/AboutView.vue
+++ vue3assign7/hello-world/src/views/AboutView.vue
@@ -2,25 +2,38 @@
   <div class="about">
     <h1>This is the movie poster page</h1>
   </div>
-  <div>
-    <img v-for="'./src/assets/Seven_Samurai_poster.jpg' in movies" v-bind:src="'./src/assets/Seven_Samurai_poster.jpg'">
-    <img v-for="'./src/assets/Ran-Vintage-Movie-Poster-Original-Japanese-1-panel-20x29.jpg' in movies" v-bind:src="'./src/assets/Ran-Vintage-Movie-Poster-Original-Japanese-1-panel-20x29.jpg'">
-    <img v-for="'./src/assets/Ikiru_poster.jpg' in movies" v-bind:src="'./src/assets/Ikiru_poster.jpg'">
-  </div>
+  <div id="app">
+    <!-- <figure v-for="movie in './src/assets/Seven_Samurai_poster.jpg'" :key="key">
+      <img v-bind:src="movie.Poster">
+      <figcaption>{{ movie.Poster }}</figcaption> -->
+      <div class="posters">
+        <img v-for="(movie, key) in movies" :key=key :alt="movie.Title" :src="movie.Poster"/>
+      </div>
+        </div>
   
   <div>
+
     <script>
 export default {
   data() {
     return {
-      movie: [
-        {Title: 'The Seven Samurai', Poster: './src/assets/Seven_Samurai_poster.jpg'},
-        {Title: 'Ikiru', Poster: './src/assets/Ran-Vintage-Movie-Poster-Original-Japanese-1-panel-20x29.jpg'},
-        {Title: 'Ran', Poster: './src/assets/Ikiru_poster.jpg'},
-    ] 
+    //   movies: [
+    //     // {Title: 'The Seven Samurai', Poster: './src/assets/Seven_Samurai_poster.jpg'},
+    //     // {Title: 'Ran', Poster: './src/assets/Ran-Vintage-Movie-Poster-Original-Japanese-1-panel-20x29.jpg'},
+    //     // {Title: 'Ikiru', Poster: './src/assets/Ikiru_poster.jpg'},
+    // ] 
+    movies: [
+        {Title: 'The Seven Samurai', Poster: require('../assets/Seven_Samurai_poster.jpg')},
+        {Title: 'Ikiru', Poster: require('../assets/Ran-Vintage-Movie-Poster-Original-Japanese-1-panel-20x29.jpg')},
+        {Title: 'Ran', Poster: require('../assets/Ikiru_poster.jpg')},
+    ]
+    };
     };
   },
-};
+
+
+app.mount('#app')
+
 </script>
   </div>
 </template>
